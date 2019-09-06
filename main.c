c9774351c0e95c86dc1e74e3b79bd049fcdd3915
Cesar Zavala <cesarz@ksu.edu>
2019-09-06 00:34:56 -0500

    printing fixed TODO: -Fix error display -Finish documentation

--- main.c
+++ main.c
@@ -5,13 +5,34 @@
 ***********************************************
 * This program validates if a specific file format and sequence of characters in the file are valid. *
 ***********************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/**********************************************
+* Description: *
+* Input: *
+* Output: *
+***********************************************/
+void printBoldError(char string[], int length, int lastOpen, int errorLoc) {
+    for (int i = 0; i < length; i++) {
+        if (i == lastOpen) printf("\e[1m%c\e[0m", string[i]);
+        else if (i == errorLoc) printf("\e[1m%c\e[0m", string[i]);
+        else printf("%c", string[i]);
+    }
+    printf("\n");
+}
+
+/**********************************************
+* Description: *Validates the operation given by checking for bracket order.
+* Input: *The operation to be validated (a string), and its length.
+* Output: *Nothing, as it only validates the operations.
+***********************************************/
 void validateOperation(char op[], int length) {
-    int position = 0; //makeshift stack counters.
+    int position, lastOpenBracket = 0; //makeshift stack counters.
     char charStack[36];
+//    char errorShow[36];
     if (length > 1) {
         printf("String is %s with length %d \n", op, length); //debug
         for (int i = 0; i < length; i++) {
@@ -20,34 +41,43 @@ void validateOperation(char op[], int length) {
                 case ' ':
                     break;
                 case '{':
+                    lastOpenBracket = i;
                     charStack[position] = '{';
                     position++;
                     break;
                 case '[':
+                    lastOpenBracket = i;
                     charStack[position] = '[';
                     position++;
                     break;
                 case '(':
+                    lastOpenBracket = i;
                     charStack[position] = '(';
                     position++;
                     break;
                 case '}':
                     if (charStack[position - 1] != '{') {
-                        printf("bracket mismatch '}'\n");
+                        //printf("bracket mismatch '}'\n"); //debug
+                        printBoldError(op, length, lastOpenBracket, i);
+                        return;
                     } else {
                         position--;
                     }
                     break;
                 case ']':
                     if (charStack[position - 1] != '[') {
-                        printf("bracket mismatch ']'\n");
+                        //printf("bracket mismatch ']'\n");
+                        printBoldError(op, length, lastOpenBracket, i);
+                        return;
                     } else {
                         position--;
                     }
                     break;
                 case ')':
                     if (charStack[position - 1] != '(') {
-                        printf("bracket mismatch ')'\n");
+                        //printf("bracket mismatch ')'\n");
+                        printBoldError(op, length, lastOpenBracket, i);
+                        return;
                     } else {
                         position--;
                     }
@@ -57,11 +87,17 @@ void validateOperation(char op[], int length) {
                     return;
             }
         }
+//        if(position > 0) printf("The operation is missing brackets (pos:%d) !\n",position);
 
     }
 //return 0;
 }
 
+/**********************************************
+* Description: *Main method that takes care of file IO and the structure validation of the file to be read.
+* Input: *It takes a file name as its only parameter.
+* Output: *Returns an integer, but its real output is the validation of every operation in the console.
+***********************************************/
 int main(int argc, char *argv[]) {
 
     if (argc < 2) { //
